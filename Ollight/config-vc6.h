263ae2a8bf281e49b5f040f22e04d58e07a30f4a
yodamaster <yodamaster@users.noreply.github.com>
2014-05-03 18:43:39 +0800

    vc6: fix build error C2065: '_tcsncpy_s' : undeclared identifier

--- Ollight/config-vc6.h
+++ Ollight/config-vc6.h
@@ -21,6 +21,83 @@
 
 #define __debugbreak() DebugBreak()
 
+
+#include <crtdbg.h>
+#include <errno.h>
+
+template <typename charT>
+inline
+int _tcsncpy_s(charT* dst, size_t size, const charT* src, size_t count)
+{
+    charT *p;
+    size_t available;
+
+    if (count == 0 && dst == NULL && size == 0)
+    {
+        /* this case is allowed; nothing to do */
+        return 0;
+    }
+
+    /* validation section */
+	_ASSERTE(dst != NULL && size > 0);
+	if(dst == NULL || size <= 0)
+	{
+		errno = EINVAL;
+		return EINVAL;
+	}
+
+    if (count == 0)
+    {
+        /* notice that the source string pointer can be NULL in this case */
+        *dst = 0;
+        return 0;
+    }
+	if(src == NULL)
+	{
+		*dst = 0;
+		_ASSERTE(src != NULL);
+		if(src == NULL)
+		{
+			errno = EINVAL;
+			return EINVAL;
+		}
+	}
+
+    p = dst;
+    available = size;
+    if (count == (size_t)-1)
+    {
+        while ((*p++ = *src++) != 0 && --available > 0)
+        {
+        }
+    }
+    else
+    {
+        while ((*p++ = *src++) != 0 && --available > 0 && --count > 0)
+        {
+        }
+        if (count == 0)
+        {
+            *p = 0;
+        }
+    }
+
+    if (available == 0)
+    {
+        if (count == (size_t)-1)
+        {
+            dst[size - 1] = 0;
+            return 80;
+        }
+        *dst = 0;
+		_ASSERTE(!L"Buffer is too small");
+		errno = ERANGE;
+		return ERANGE;
+
+    }
+    return 0;
+}
+
 #endif // _MSC_VER < 1200
 
 #endif // _CONFIG_VC6_H_
